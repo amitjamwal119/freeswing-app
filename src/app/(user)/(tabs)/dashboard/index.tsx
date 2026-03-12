a0cb9836dbb07ebe4d68cc14c0a85dbedf066a64
Kunal39537 <kunal@mandavconsultancy.com>
2026-03-12 14:05:12 +0530

    changes done

--- src/app/(user)/(tabs)/dashboard/index.tsx
+++ src/app/(user)/(tabs)/dashboard/index.tsx
@@ -1,15 +1,13 @@
 import { Badge } from "@/components/badge";
 import { Box } from "@/components/box";
 import { HStack } from "@/components/hstack";
-import { Text } from "@/components/text";
-import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from "@/components/themed-text";
 import { VStack } from "@/components/vstack";
 import { Spacing } from '@/constants/theme';
 import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, Pressable, ScrollView, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Pressable, ScrollView, StyleSheet, View, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 import { HistoryTab, type GameHistory } from "./tabs/HistoryTab";
 import { InProgressTab, type InProgressGame } from "./tabs/InProgressTab";
 import { OverviewTab, type Scorecard } from "./tabs/OverviewTab";
@@ -152,6 +150,8 @@ export default function DashboardScreen() {
     { key: "progress", label: "In Progress", icon: "hourglass-outline" },
     { key: "history", label: "Game History", icon: "time-outline" },
   ];
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === "dark";
 
   useEffect(() => {
     setPlayerName('Kunal');
@@ -159,165 +159,182 @@ export default function DashboardScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView style={{ flex: 1 }}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.scrollContent}
-      >
-        <ThemedView style={styles.container}>
-          {/* <ThemedText>Player Dashboard</ThemedText> */}
-          {loading ? (
-            <ActivityIndicator size="small" color="#dc2626" />
-          ) : (
-            <VStack className="mt-2">
-              <Text className="text-gray-500">Welcome back 👋</Text>
-              <Text className="text-3xl font-bold">Kunal</Text>
-            </VStack>
-          )}
-          {/* Tabs */}
-          <View className="flex-row items-center bg-gray-100 p-1 rounded-full justify-between">
-            {tabs.map((tab) => {
-              const active = activeTab === tab.key;
-              return (
-                <Pressable
-                  key={tab.key}
-                  onPress={() => setActiveTab(tab.key)}
-                  className={`flex-row items-center px-4 py-2 rounded-full ${active ? "bg-[#8BC34A]" : ""
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: isDark ? "#000" : "#f2f2f2",
+      }}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* <ThemedText>Player Dashboard</ThemedText> */}
+        {loading ? (
+          <ActivityIndicator size="small" color="#dc2626" />
+        ) : (
+          <VStack className="mt-2">
+            <ThemedText
+              style={{
+                fontSize: 14,
+
+                marginTop: 8,
+
+              }}
+            >
+              Welcome back 👋
+            </ThemedText>
+            {/* <ThemedText 
+             style={{
+                fontSize: 14,
+                //opacity: 0.6,
+                marginTop: 8,
+                textAlign: "start",
+              }}
+              >Welcome back 👋</ThemedText> */}
+            <ThemedText className="text-3xl font-bold">Kunal</ThemedText>
+          </VStack>
+        )}
+        {/* Tabs */}
+        <View className="flex-row items-center bg-gray-100 p-1 rounded-full justify-between">
+          {tabs.map((tab) => {
+            const active = activeTab === tab.key;
+            return (
+              <Pressable
+                key={tab.key}
+                onPress={() => setActiveTab(tab.key)}
+                className={`flex-row items-center px-4 py-2 rounded-full ${active ? "bg-[#8BC34A]" : ""
+                  }`}
+              >
+                <ThemedText
+                  className={`text-sm font-medium ${active ? "text-white" : "text-gray-500"
                     }`}
                 >
-                  <Text
-                    className={`text-sm font-medium ${active ? "text-white" : "text-gray-500"
-                      }`}
-                  >
-                    {tab.label}
-                  </Text>
-                </Pressable>
-              );
-            })}
-          </View>
+                  {tab.label}
+                </ThemedText>
+              </Pressable>
+            );
+          })}
+        </View>
 
-          {activeTab === "overview" && (
-            <>
-              <VStack space="md">
-                {/* Row 1 */}
-                <HStack space="md">
+        {activeTab === "overview" && (
+          <>
+            <VStack space="md">
+              {/* Row 1 */}
+              <HStack space="md">
 
-                  {/* Card 1 */}
-                  <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
+                {/* Card 1 */}
+                <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
 
-                    <HStack className="justify-between items-center">
+                  <HStack className="justify-between items-center">
 
-                      <VStack className="space-y-1">
+                    <VStack className="space-y-1">
 
-                        <Text className="text-3xl font-bold">3</Text>
-                        <Text className="text-sm text-gray-500 font-medium">
-                          COURSES PLAYED
-                        </Text>
-                        <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
-                          <Text className="text-xs">Unique</Text>
-                        </Badge>
-                      </VStack>
-                      <Box className="bg-amber-100 p-3 rounded-full items-center justify-center mb-10">
-                        <Ionicons name="location" size={15} color="#d97706" />
-                      </Box>
-                    </HStack>
-                  </Box>
+                      <ThemedText className="text-3xl font-bold">3</ThemedText>
+                      <ThemedText className="text-sm text-gray-500 font-medium">
+                        COURSES PLAYED
+                      </ThemedText>
+                      <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
+                        <ThemedText className="text-xs">Unique</ThemedText>
+                      </Badge>
+                    </VStack>
+                    <Box className="bg-amber-100 p-3 rounded-full items-center justify-center mb-10">
+                      <Ionicons name="location" size={15} color="#d97706" />
+                    </Box>
+                  </HStack>
+                </Box>
 
-                  {/* Card 2 */}
-                  <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
-                    <HStack className="justify-between items-center">
-                      <VStack className="space-y-1">
-                        <Text className="text-3xl font-bold">72.300</Text>
-                        <Text className="text-sm text-gray-500 font-medium">
-                          AVG SCORE
-                        </Text>
-                        <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
-                          <Text className="text-xs">Per 18 Holes</Text>
-                        </Badge>
-                      </VStack>
-                      <Box className="bg-blue-100 p-3 rounded-full items-center justify-center mb-10">
-                        <Ionicons name="stats-chart-outline" size={15} color="#06abd4" />
-                      </Box>
-                    </HStack>
-                  </Box>
-                </HStack>
+                {/* Card 2 */}
+                <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
+                  <HStack className="justify-between items-center">
+                    <VStack className="space-y-1">
+                      <ThemedText className="text-3xl font-bold">72.300</ThemedText>
+                      <ThemedText className="text-sm text-gray-500 font-medium">
+                        AVG SCORE
+                      </ThemedText>
+                      <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
+                        <ThemedText className="text-xs">Per 18 Holes</ThemedText>
+                      </Badge>
+                    </VStack>
+                    <Box className="bg-blue-100 p-3 rounded-full items-center justify-center mb-10">
+                      <Ionicons name="stats-chart-outline" size={15} color="#06abd4" />
+                    </Box>
+                  </HStack>
+                </Box>
+              </HStack>
 
-                {/* Row 2 */}
-                <HStack space="md">
-                  {/* Card 3 */}
-                  <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
-                    <HStack className="justify-between items-center">
-                      <VStack className="space-y-1">
-                        <Text className="text-3xl font-bold">40</Text>
-                        <Text className="text-sm text-gray-500 font-medium">
-                          BEST SCORE
-                        </Text>
-                        <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
-                          <Text className="text-xs">Personal Best</Text>
-                        </Badge>
-                      </VStack>
-                      <Box className="bg-green-100 p-3 rounded-full items-center justify-center mb-10">
-                        <Ionicons name="star" size={15} color="#06d428" />
-                      </Box>
-                    </HStack>
-                  </Box>
+              {/* Row 2 */}
+              <HStack space="md">
+                {/* Card 3 */}
+                <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
+                  <HStack className="justify-between items-center">
+                    <VStack className="space-y-1">
+                      <ThemedText className="text-3xl font-bold">40</ThemedText>
+                      <ThemedText className="text-sm text-gray-500 font-medium">
+                        BEST SCORE
+                      </ThemedText>
+                      <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
+                        <ThemedText className="text-xs">Personal Best</ThemedText>
+                      </Badge>
+                    </VStack>
+                    <Box className="bg-green-100 p-3 rounded-full items-center justify-center mb-10">
+                      <Ionicons name="star" size={15} color="#06d428" />
+                    </Box>
+                  </HStack>
+                </Box>
 
-                  {/* Card 4 */}
-                  <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
-                    <HStack className="justify-between items-center">
-                      <VStack className="space-y-1">
-                        <Text className="text-3xl font-bold">15</Text>
-                        <Text className="text-sm text-gray-500 font-medium">
-                          HANDICAP INDEX
-                        </Text>
-                        <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
-                          <Text className="text-xs">Portable Index</Text>
-                        </Badge>
-                      </VStack>
-                      <Box className="bg-green-100 p-3 rounded-full items-center justify-center mb-10">
-                        <Ionicons name="flag" size={15} color="#2fe228ff" />
-                      </Box>
-                    </HStack>
-                  </Box>
-                </HStack>
-              </VStack>
+                {/* Card 4 */}
+                <Box className="flex-1 bg-white p-4 rounded-xl border border-gray-200">
+                  <HStack className="justify-between items-center">
+                    <VStack className="space-y-1">
+                      <ThemedText className="text-3xl font-bold">15</ThemedText>
+                      <ThemedText className="text-sm text-gray-500 font-medium">
+                        HANDICAP INDEX
+                      </ThemedText>
+                      <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
+                        <ThemedText className="text-xs">Portable Index</ThemedText>
+                      </Badge>
+                    </VStack>
+                    <Box className="bg-green-100 p-3 rounded-full items-center justify-center mb-10">
+                      <Ionicons name="flag" size={15} color="#2fe228ff" />
+                    </Box>
+                  </HStack>
+                </Box>
+              </HStack>
+            </VStack>
 
-              {/* Home Course Handicap */}
-              <Box className="w-full bg-white p-4 rounded-xl border border-gray-200 mt-3">
-                <HStack className="justify-between items-center">
-                  <VStack className="space-y-1">
-                    <Text className="text-3xl font-bold">0</Text>
-                    <Text className="text-sm text-gray-500 font-medium">
-                      HOME COURSE HANDICAP
-                    </Text>
-                    <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
-                      <Text className="text-xs">No Home Course</Text>
-                    </Badge>
-                  </VStack>
-                  <Box className="bg-gray-100 p-4 rounded-full items-center justify-center">
-                    <Ionicons name="home" size={22} color="#757575ff" />
-                  </Box>
-                </HStack>
-              </Box>
-            </>
-          )}
+            {/* Home Course Handicap */}
+            <Box className="w-full bg-white p-4 rounded-xl border border-gray-200 mt-3">
+              <HStack className="justify-between items-center">
+                <VStack className="space-y-1">
+                  <ThemedText className="text-3xl font-bold">0</ThemedText>
+                  <ThemedText className="text-sm text-gray-500 font-medium">
+                    HOME COURSE HANDICAP
+                  </ThemedText>
+                  <Badge className="bg-gray-200 rounded-md self-start px-2 py-1">
+                    <ThemedText className="text-xs">No Home Course</ThemedText>
+                  </Badge>
+                </VStack>
+                <Box className="bg-gray-100 p-4 rounded-full items-center justify-center">
+                  <Ionicons name="home" size={22} color="#757575ff" />
+                </Box>
+              </HStack>
+            </Box>
+          </>
+        )}
 
-          {/* Tab Content */}
-          {activeTab === "overview" && <OverviewTab cards={cards} handleLike={handleLike} />}
-          {activeTab === "progress" && (
-            <InProgressTab
-              games={inProgressGames}
-              onDelete={handleDeleteInProgress}
-              onResume={handleResumeInProgress}
-            />
-          )}
-          {activeTab === "history" && (
-            <HistoryTab
-              games={gameHistory}
-              onViewGame={handleViewHistory}
-            />
-          )}
-        </ThemedView>
+        {/* Tab Content */}
+        {activeTab === "overview" && <OverviewTab cards={cards} handleLike={handleLike} />}
+        {activeTab === "progress" && (
+          <InProgressTab
+            games={inProgressGames}
+            onDelete={handleDeleteInProgress}
+            onResume={handleResumeInProgress}
+          />
+        )}
+        {activeTab === "history" && (
+          <HistoryTab
+            games={gameHistory}
+            onViewGame={handleViewHistory}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
